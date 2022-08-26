a4132d2c277adcc6964b6d64b1be82fcaa7e1dc0
Douglas Lopes <pchialoopes@gmail.com>
2022-08-26 16:24:57 -0300

    Estilizando a page login

--- src/components/login/ImagemComponent.js
+++ src/components/login/ImagemComponent.js
@@ -1,7 +1,7 @@
 import styles from '../login/LoginComponent.module.css';
 
 function ImagemComponent(){
-    return <img src="/logo.png" className={styles.imageLogin}></img>;
+    return <img src="/logo.png" alt='logo' className={styles.imageLogin}></img>;
 }
 
 export default ImagemComponent
--- src/components/login/LoginComponent.js
+++ src/components/login/LoginComponent.js
@@ -4,18 +4,18 @@ import ImagemComponent from './ImagemComponent';
 function LoginComponent (){
 
     return(
-        <div class="d-flex">
+        <div className={styles.containerLogin}>
             <div>
                 <ImagemComponent className={styles.imageLogin}/>
             </div>
             <div className={styles.container}>
                 <div className={styles.titulo}>
-                    <img src="/marca_lojistas.svg" className={styles.svgLogin}></img> <br></br>
-                    <span><strong>Acesse com seu email e senha abaixo</strong></span>
+                    <img src="/marca_lojistas.svg" alt='marca lojistas' className={styles.svgLogin}></img> <br></br>
+                    <span class="d-none d-sm-block"><strong>Acesse com seu email e senha abaixo</strong></span>
                 </div>
                 <form class="m-4">
                     <div class="mb-3">
-                        <label class="form-label">Email</label>
+                        <label class="form-label">Seu e-mail</label>
                         <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Seu email"></input>
                     </div>
                     <div class="mb-3">
@@ -27,7 +27,7 @@ function LoginComponent (){
                     </div>
                 </form>
                 <div>
-                    <p className={styles.footerLogin}>
+                    <p class="d-none d-sm-block fw-lighter fs-6 text-sm-start p-2">
                         © 2021 Caffeine Army. CNPJ: 27.403.527/0001-13 <br></br> Endereço: Rua Rubens Guelli, 68, Itaigara, Salvador - BA, 41815-135 <br></br> E-mail: contato@caffeinearmy.com.br | Telefone: +55 11 91106-4910
                     </p>
                 </div>
