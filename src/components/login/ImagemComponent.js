4a5237ed175bf8405ade9fa40bbc25498d9901c5
Douglas Lopes <pchialoopes@gmail.com>
2022-08-28 15:17:50 -0300

    Arquivos deletados

--- src/components/login/ImagemComponent.js
+++
@@ -1,7 +0,0 @@
-import styles from '../login/LoginComponent.module.css';
-
-function ImagemComponent(){
-    return <img src="/logo.png" alt='logo' className={styles.imageLogin}></img>;
-}
-
-export default ImagemComponent
