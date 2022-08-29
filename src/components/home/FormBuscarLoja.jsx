2a0b33751236d16d2f2a6f43fb891344cb0b361e
Douglas Lopes <pchialoopes@gmail.com>
2022-08-28 22:48:49 -0300

    Adicionando props

---
+++ src/components/home/FormBuscarLoja.jsx
@@ -0,0 +1,27 @@
+import { FaArrowRight } from 'react-icons/fa'
+
+export default function FormBuscarLoja(props){
+    return (
+        <div>
+             <div className="d-flex justify-content-evenly align-items-center">
+                <form className="form-control"  onSubmit={props.formSubmit}>
+                    <div className="input-group">
+                        <div className="input-group mb-3">
+                            <input 
+                                type="text"
+                                className="form-control w-75"
+                                name="cnpj"
+                                onChange={(e) => props.setCnpj(e.target.value)}
+                                placeholder="Digite um CNPJ"
+                            />
+                            <button 
+                            className="btn btn-secondary d-sm-block d-md-none m-2"
+                            ><FaArrowRight/></button>
+                            <button className="btn btn-danger d-none d-md-block d-lg-block m-2">Buscar Loja</button>
+                        </div>
+                    </div>     
+                </form>   
+            </div>
+        </div>
+    )
+}
--- src/components/home/HomeComponent.jsx
+++ src/components/home/HomeComponent.jsx
@@ -1,4 +1,4 @@
-import { FaArrowRight } from 'react-icons/fa'
+
 import api from "../../api/api";
 import { useState } from "react";
 import NavBar from "../layouts/NavBar";
@@ -6,6 +6,7 @@ import style from '../home/HomeComponent.module.css'
 import CollapseLojista from "./collapse/collapseLojista";
 import CollapseEmpresa from "./collapse/collapseEmpresa";
 import ConteudoHome from "./conteudo/conteudoHome";
+import FormBuscarLoja from "./FormBuscarLoja";
 
 export default function HomeComponent(){
 
@@ -25,22 +26,12 @@ export default function HomeComponent(){
             <NavBar></NavBar>
             <div class="text-danger border-bottom border-muted"></div>
             <div class="d-sm" className={style.background}>
-                <div className={style.content}>
-                    <div class="d-flex justify-content-evenly align-items-center">
-                        <form class="form-control"  onSubmit={formSubmit}>
-                            <div class="input-group">
-                                <div class="input-group mb-3">
-                                    <input type="text" class="form-control w-75" name="cnpj" onChange={(e) => setCnpj(e.target.value)} placeholder="Digite um CNPJ"></input>
-                                    <button class="btn btn-secondary d-sm-block d-md-none m-2"><FaArrowRight/></button>
-                                    <button class="btn btn-danger d-none d-md-block d-lg-block m-2">Buscar Loja</button>
-                                </div>
-                            </div>     
-                        </form>   
-                    </div>
-                    <ConteudoHome></ConteudoHome>
+                <div className={style.content}>  
+                    <FormBuscarLoja formSubmit={formSubmit} setCnpj={setCnpj}/>              
+                    <ConteudoHome />
                 </div>
-                <CollapseLojista empresa={empresa}></CollapseLojista>
-                <CollapseEmpresa empresa={empresa}></CollapseEmpresa>
+                <CollapseLojista empresa={empresa}/>
+                <CollapseEmpresa empresa={empresa}/>
             </div>
         </div>
     )
