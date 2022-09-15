cd2f69b1fef89e8e6216caecb11c5a475444acba
Douglas Lopes <pchialoopes@gmail.com>
2022-09-15 19:07:40 -0300

    Update rotas privadas

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getToken } from '../services/auth';
 
 const api = axios.create({
-  baseURL: `http://localhost:3001`
+  baseURL: `http://localhost:3001/`
 });
 
 api.interceptors.request.use(async config => {
---
+++ src/routes/index.jsx
@@ -0,0 +1,34 @@
+import { Switch, Route, Redirect } from 'react-router-dom'
+import { isAuthenticated } from '../services/auth';
+import  HomeComponent from '../components/home/HomeComponent';
+import  LoginComponent from '../components/login/LoginComponent';
+import  NotFound from '../components/not-found/NotFound';
+
+export function Rotas() {
+
+  const PrivateRoute = ({ component: Component, ...rest}) => (
+    <Route
+      {...rest}
+      render ={props =>
+        isAuthenticated() ? (
+          <Component {...props}/> 
+        ) : (
+          <Redirect 
+            to={{
+              pathname: '/',
+              state: { from: props.location },
+            }}
+          />
+        )
+      }
+    />
+  );
+    
+  return (
+    <Switch>
+      <Route exact path='/' component={LoginComponent} />
+      <PrivateRoute exact path='/home' component={HomeComponent}/>
+      <Route exact path='*' component={NotFound}/>
+    </Switch>
+  );
+}
