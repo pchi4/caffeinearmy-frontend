fe016f05efa310963e9aaef7d0d39ea0fbc383b8
Douglas Lopes <pchialoopes@gmail.com>
2022-09-16 16:26:06 -0300

    Configurando a rota not-found

--- src/components/not-found/NotFound.jsx
+++ src/components/not-found/NotFound.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 export default function NotFound(){
+
+    let history = useHistory();
+
+    function redirectTo(e){
+        e.preventDefault();
+
+        history.push('/')
+    }
+
     return(
-        <h1>Not found</h1>
+       <div className="text-dark">
+            <div className="container text-center vh-100 m-auto py-5">
+                <div className="display-1"><strong>404</strong></div>
+                <img src="/not-found.jpg" className="img-fluid mt-5" alt="error"  />
+                <h5 className="text-dark mt-5">Page not found</h5>
+                <div className="mt-5"><button className="btn btn-info" onClick={redirectTo} >Home Page</button></div>
+            </div>
+       </div>
     )
 }
