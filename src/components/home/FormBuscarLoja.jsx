3aaefea5e04f1214d882fffd6e7435669c69c585
Douglas Lopes <pchialoopes@gmail.com>
2022-10-20 12:22:56 -0300

    Adcionando o icone no input e estilizando

--- src/components/home/FormBuscarLoja.jsx
+++ src/components/home/FormBuscarLoja.jsx
@@ -1,22 +1,35 @@
-import { FaArrowRight } from 'react-icons/fa'
+import { useState } from 'react'
+import { FaArrowRight, FaSearch } from 'react-icons/fa'
 import styles from '../home/HomeStyle.module.css'
 
 export default function FormBuscarLoja(props){
+    const [target, setTarget] = useState();
+    
+    function noneInput(e){
+        var teste = e.target.value.length
+        setTarget(teste)
+    }
+
     return (
         <div className={styles.backMobile}>
              <div class='d-flex justify-content-evenly align-items-center'>
                 <form className='w-100'  onSubmit={props.formSubmit}>
                     <div className='input-group p-3'>
                         <div className='input-group mb-3'>
+                                {
+                                    target >= 1?
+
+                                    <i className={styles.iconNone}></i> : <i><FaSearch className={styles.iconInput} color="#dc3545"/></i>
+                                }
                             <input 
                                 type='text'
                                 className='form-control w-75'
                                 name='cnpj'
                                 onChange={(e) => props.setCnpj(e.target.value)}
-                                placeholder='Digite um CNPJ'
+                                placeholder='         Digite um CNPJ'
+                                onKeyDown={noneInput}
                             />
-                            <button 
-                            className='btn btn-secondary d-sm-block d-md-none m-2'
+                            <button className='btn btn-secondary d-sm-block d-md-none m-2'
                             ><FaArrowRight/></button>
                             <button className='btn btn-danger d-none d-md-block d-lg-block m-2'>Buscar Loja</button>
                         </div>
