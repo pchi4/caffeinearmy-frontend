934a25d74504a410d46d12466c005247bc604662
Douglas Santos <douglas.santos@ssp.ba.gov.br>
2023-02-10 08:55:28 -0300

    Adicionando o inteceptor para resquest

--- src/api/api.js
+++ src/api/api.js
@@ -9,12 +9,26 @@ const api = axios.create({
   baseURL: "http://localhost:3001",
 });
 
-api.interceptors.request.use(async (config) => {
-  const token = getToken();
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+api.interceptors.request.use(
+  async (config) => {
+    const token = getToken();
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
   }
-  return config;
-});
+);
+
+axios.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
 
 export default api;
