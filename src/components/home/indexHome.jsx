c7158fd661af5f0a6405120f070c439db7671628
Douglas Santos <douglas.santos@ssp.ba.gov.br>
2023-02-10 08:58:44 -0300

    Tratando erro ao buscar empresa

--- src/components/home/indexHome.jsx
+++ src/components/home/indexHome.jsx
@@ -1,25 +1,35 @@
 import api from "../../api/api";
-import React, { useState } from "react";
+import { useState } from "react";
 import NavBar from "../layouts/NavBar";
 import style from "../home/HomeStyle.module.css";
 import CollapseLojista from "./collapse/collapseLojista";
 import CollapseEmpresa from "./collapse/collapseEmpresa";
 import ConteudoHome from "./conteudo/conteudoHome";
 import FormBuscarLoja from "./FormBuscarLoja";
+import { Alert } from 'react-bootstrap'
 
 export default function indexHome() {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
   const [cnpj, setCnpj] = useState();
+  // eslint-disable-next-line react-hooks/rules-of-hooks
   const [empresa, setEmpresa] = useState([]);
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [mensageErro, setMensageErro] = useState([undefined, false])
 
-  function formSubmit(event) {
-    event.preventDefault();
-    const regexCnpj = cnpj.replace(/[^\d]+/g, "");
+  async function formSubmit() {
+    console.log(cnpj)
+
+    // const regexCnpj = await cnpj.replace(/[^\d]+/g, "");
 
     try {
-      const res = api.get(`/empresa/${regexCnpj}`)
-      setEmpresa(res.data)
-    } catch (erro) {
-      console.log(erro)
+      const res = await api.get(`/empresa/${cnpj}`);
+      setEmpresa(res.data);
+    } catch (error) {
+      console.log(error.response);
+      setMensageErro(error.response.data.message, true)
+      setTimeout(() => {
+        setMensageErro('', false)
+      }, 2000)
     }
   }
 
@@ -30,6 +40,7 @@ export default function indexHome() {
       </div>
       <div className="text-danger border-bottom border-muted"></div>
       <div class="d-sm" className={style.background}>
+        {mensageErro && <Alert variant="danger">{mensageErro}</Alert>}
         <div className={style.content}>
           <FormBuscarLoja formSubmit={formSubmit} setCnpj={setCnpj} />
           <ConteudoHome />
