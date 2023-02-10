087b696fd4253397fba46139fb76f4e183cc659f
Douglas Santos <douglas.santos@ssp.ba.gov.br>
2023-02-10 08:57:45 -0300

    Refatorando o tratamento de erro

--- src/components/home/modal/ModalCadastroUser.jsx
+++ src/components/home/modal/ModalCadastroUser.jsx
@@ -9,23 +9,26 @@ export default function ModalCadastroUser() {
   const handleShow = () => setShow(true);
   const [showAlert, setAlert] = useState(false);
   const [showForm, setForm] = useState(true);
-  const { register, handleSubmit, formState: { errors } } = useForm();
-  const registerUser = data => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const registerUser = (data) => {
     try {
-      api.post(`/usuario/cadastrar`, data)
+      api.post(`/usuario/cadastrar`, data);
       setAlert(true);
-      setForm(false)
+      setForm(false);
 
       setTimeout(() => {
         setAlert(false);
         handleClose();
-        setForm(true)
+        setForm(true);
       }, 3000);
-
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   return (
     <>
@@ -38,62 +41,75 @@ export default function ModalCadastroUser() {
           <Modal.Title>Cadastro de Usuário</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          {showForm && (<Form>
-            <Form.Group className="mb-3">
-              <Form.Label className="form-label">
-                Nome:
-              </Form.Label>
-              <Form.Control
-                type="text"
-                className="form-control"
-                placeholder="Seu nome"
-                {...register("name", { required: "Nome é obrigatório" })}
-                aria-invalid={errors.name ? "true" : "false"}
-              />
-              {errors.name && <p className="text-danger my-1" role="alert"><strong>{errors.name?.message}</strong></p>}
-            </Form.Group>
-            <Form.Group className="mb-3">
-              <Form.Label className="form-label">
-                Email:{" "}
-              </Form.Label>
-              <Form.Control
-                type="email"
-                className="form-control"
-                placeholder="name@example.com"
-                {...register("email", { required: "Email é obrigatório" })}
-                aria-invalid={errors.email ? "true" : "false"}
-              />
-              {errors.email && <p className="text-danger my-1" role="alert"><strong>{errors.email?.message}</strong></p>}
-            </Form.Group>
-            <Form.Group class="mb-3">
-              <Form.Label className="form-label">
-                Telefone:{" "}
-              </Form.Label>
-              <Form.Control
-                type="text"
-                class="form-control"
-                placeholder="(xx) x-xxxx-xxxx"
-              />
-            </Form.Group>
-            <Form.Group className="mb-3">
-              <Form.Label className="form-label">
-                Senha
-              </Form.Label>
-              <Form.Control
-                type="password"
-                className="form-control"
-                placeholder="Sua senha"
-                {...register("password", {
-                  required: "Senha é obrigatório",
-                  minLength: {
-                    value: 8,
-                    message: "Senha muito pequena"
-                  }
-                })}
-              />
-              <p className="text-danger my-1" role="alert"><strong>{errors.password?.message}</strong></p>
-            </Form.Group>
-          </Form>)}
+          {showForm && (
+            <Form>
+              <Form.Group className="mb-3">
+                <Form.Label className="form-label">Nome*:</Form.Label>
+                <Form.Control
+                  type="text"
+                  className="form-control"
+                  placeholder="Seu nome"
+                  {...register("name", { required: "Nome é obrigatório" })}
+                  aria-invalid={errors.name ? "true" : "false"}
+                />
+                {errors.name && (
+                  <p className="text-danger my-1" role="alert">
+                    <strong>{errors.name?.message}</strong>
+                  </p>
+                )}
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label className="form-label">Email*: </Form.Label>
+                <Form.Control
+                  type="email"
+                  className="form-control"
+                  placeholder="name@example.com"
+                  {...register("email", { required: "Email é obrigatório" })}
+                  aria-invalid={errors.email ? "true" : "false"}
+                />
+                {errors.email && (
+                  <p className="text-danger my-1" role="alert">
+                    <strong>{errors.email?.message}</strong>
+                  </p>
+                )}
+              </Form.Group>
+              <Form.Group class="mb-3">
+                <Form.Label className="form-label">Telefone*: </Form.Label>
+                <Form.Control
+                  type="text"
+                  class="form-control"
+                  placeholder="(xx) x-xxxx-xxxx"
+                  {...register("telefone", {
+                    required: "Telefone é obrigatório",
+                  })}
+                  aria-invalid={errors.telefone ? "true" : "false"}
+                />
+                {errors.telefone && (
+                  <p className="text-danger my-1" role="alert">
+                    <strong>{errors.telefone?.message}</strong>
+                  </p>
+                )}
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label className="form-label">Senha*:</Form.Label>
+                <Form.Control
+                  type="password"
+                  className="form-control"
+                  placeholder="Sua senha"
+                  {...register("password", {
+                    required: "Senha é obrigatório",
+                    minLength: {
+                      value: 8,
+                      message: "Senha muito pequena",
+                    },
+                  })}
+                />
+                <p className="text-danger my-1" role="alert">
+                  <strong>{errors.password?.message}</strong>
+                </p>
+              </Form.Group>
+            </Form>
+          )}
           {showAlert && (
             <Alert variant="success">Cadastro realizado com sucesso!</Alert>
           )}
